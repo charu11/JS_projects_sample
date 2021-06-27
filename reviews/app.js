b4cad0739a41e7c9f5d95a3303bafaeecb90a727
charun umesh <charunumesh@gmail.com>
2021-06-28 00:27:21 +0530

    reviews completed

--- reviews/app.js
+++ reviews/app.js
@@ -67,10 +67,23 @@ function showPerson(person){
 
 nextBtn.addEventListener("click", function (){
     currentItem ++;
+    if (currentItem > reviews.length -1){
+        currentItem = 0;
+    }
     showPerson(currentItem);
 })
 
-prevBtn.addEventListener("click", function(){
+prevBtn.addEventListener("click", function (){
     currentItem --;
+    if (currentItem < 0 ){
+        currentItem = reviews.length -1;
+    }
+    showPerson(currentItem);
+})
+
+// show random review
+
+randomBtn.addEventListener("click", function(){
+    currentItem = Math.floor(Math.random() * reviews.length);
     showPerson(currentItem);
 })
